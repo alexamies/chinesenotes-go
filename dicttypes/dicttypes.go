9565427cfe9fe2325211d2288c02f532e99e324b
jifusfo <jifusfo@outlook.com>
2020-10-04 18:15:45 -0700

    New functions on dicttypes

--- dicttypes/dicttypes.go
+++ dicttypes/dicttypes.go
@@ -16,6 +16,10 @@
 //
 package dicttypes
 
+import (
+	"strings"
+)
+
 // A top level word structure that may include multiple word senses
 type Word struct {
 	Simplified, Traditional, Pinyin string
@@ -41,6 +45,33 @@ func CloneWord(w Word) Word {
 	}
 }
 
+// Tests whether the word is a function word
+func (ws *WordSense) IsFunctionWord() bool {
+	functionPOS := map[string]bool{
+		"adverb": true,
+		"conjunction": true,
+		"interjection": true,
+		"interrogative pronoun": true,
+		"measure word": true,
+		"particle": true,
+		"prefix": true,
+		"preposition": true,
+		"pronoun": true,
+		"suffix": true,
+	}	
+	functionalWords := map[string]bool{
+		"有": true,
+		"是": true,
+		"诸": true,
+		"故": true,
+		"出": true,
+		"当": true,
+		"若": true,
+		"如": true,
+	}
+	return functionalWords[ws.Simplified] || functionPOS[ws.Grammar]
+}
+
 // IsProperNoun tests whether the word is a proper noun.
 // If the majority of word senses are proper nouns, then the word is marked
 // as a proper noun.
@@ -52,4 +83,79 @@ func IsProperNoun(w *Word) bool {
 		}
 	}
 	return float64(count) / float64(len(w.Senses)) > 0.5
+}
+
+// May be sorted into descending order with most frequent bigram first
+type words []Word
+
+
+func (hwArr words) Len() int {
+	return len(hwArr)
+}
+
+func (hwArr words) Swap(i, j int) {
+	hwArr[i], hwArr[j] = hwArr[j], hwArr[i]
+}
+
+func (hwArr words) Less(i, j int) bool {
+	noTones1 := normalizePinyin(hwArr[i].Pinyin)
+	noTones2 := normalizePinyin(hwArr[j].Pinyin)
+	return noTones1 < noTones2
+}
+
+// Removes the tone diacritics from a Pinyin string
+func normalizePinyin(pinyin string) string {
+	runes := []rune{}
+	for _, r := range pinyin {
+		n, ok := NORMAL[r]
+		if ok {
+			runes = append(runes, n)
+		} else {
+			runes = append(runes, r)
+		}
+	}
+	return strings.ToLower(string(runes))
+}
+
+var NORMAL = map[rune]rune{
+	'ā': 'a',
+	'á': 'a',
+	'ǎ': 'a',
+	'à': 'a',
+	'ē': 'e',
+	'é': 'e',
+	'ě': 'e',
+	'è': 'e',
+	'ī': 'i',
+	'í': 'i',
+	'ǐ': 'i',
+	'ì': 'i',
+	'ō': 'o',
+	'ó': 'o',
+	'ǒ': 'o',
+	'ò': 'o',
+	'ū': 'u',
+	'ú': 'u',
+	'ǔ': 'u',
+	'ù': 'u',
+	'Ā': 'a',
+	'Á': 'a',
+	'Ǎ': 'a',
+	'À': 'a',
+	'Ē': 'e',
+	'É': 'e',
+	'Ě': 'e',
+	'È': 'e',
+	'Ī': 'i',
+	'Í': 'i',
+	'Ǐ': 'i',
+	'Ì': 'i',
+	'Ō': 'o',
+	'Ó': 'o',
+	'Ǒ': 'o',
+	'Ò': 'o',
+	'Ū': 'u',
+	'Ú': 'u',
+	'Ǔ': 'u',
+	'Ù': 'u',
 }
