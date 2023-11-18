428be594c9c1002e600fc240c2ed2d5dbff79544
alexamies <alexamies@gmail.com>
2023-11-18 15:55:13 -0800

    Reduced logging

--- bibnotes/bibnotes.go
+++ bibnotes/bibnotes.go
@@ -171,12 +171,12 @@ func (client bibNotesClient) GetParallelRefs(fileName string) []ParellelRef {
 func (client bibNotesClient) GetTransRefs(fileName string) []TransRef {
 	refNo, ok := client.file2Ref[fileName]
 	if !ok {
-		log.Printf("GetTransRefs: no value for fileName = %s", fileName)
+		// log.Printf("GetTransRefs: no value for fileName = %s", fileName)
 		return []TransRef{}
 	}
 	transRefs, ok := client.refNo2Trans[refNo]
 	if !ok {
-		log.Printf("GetTransRefs: no value for refNo = %s", refNo)
+		// log.Printf("GetTransRefs: no value for refNo = %s", refNo)
 		return []TransRef{}
 	}
 	return transRefs
